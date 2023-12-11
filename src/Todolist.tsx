4b6448adf2095f655cfb9a371a70316afcbb4181
demianov-an <Alexm92@inbox.ru>
2023-12-11 13:21:42 +0600

    lesson-7 edited the 'input type=checkbox' with a Checkbox

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -2,7 +2,7 @@ import React, {ChangeEvent} from 'react'
 import {FilterValuesType} from './App'
 import {AddItemForm} from './AddItemForm';
 import {EditableSpan} from './EditableSpan';
-import {Button, IconButton} from '@mui/material';
+import {Button, Checkbox, IconButton} from '@mui/material';
 import Delete from '@mui/icons-material/Delete';
 
 export type TaskType = {
@@ -58,8 +58,8 @@ export function Todolist(props: PropsType) {
           }
           return (
             <li key={t.id} className={t.isDone ? 'is-done' : ''}>
-              <input
-                type="checkbox"
+              <Checkbox
+                color="secondary"
                 checked={t.isDone}
                 onChange={onChangeHandler}
               />
